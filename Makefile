5211e41fa6a1c3f715b6ec50397cd6a5bf5f93d8
Andrew Jenkins <andrewjjenkins@gmail.com>
2025-02-09 12:43:40 -0700

    HomeAssistant stand v1.0

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 
 .PHONY: all clean
 
-all: out/stand.scad
+all: out/stand.scad out/stand.stl
 
 out/stand.scad: stand/stand.py
 	mkdir -p out
 	python $<
+
+out/stand.stl: out/stand.scad
+	openscad -o $@ --export-format binstl $<
 
 clean:
 	rm -rf out/
--- stand/stand.py
+++ stand/stand.py
@@ -1,11 +1,28 @@
 from solid import *
 from solid.utils import *
 
-b = difference()(
-    cube(10),
-    sphere(15),
+boxes = import_scad("mcad/boxes.scad")
+
+base = color([1, 1, 0], 0.3)(boxes.roundedCube([90, 130, 15], 3, False, True))
+
+rise = translate([-16.9, 0, 39])(
+    rotate([0, -60, 0])(boxes.roundedCube([90, 130, 15], 3, False, True))
 )
 
-if __name__ == '__main__':
-    file_out = scad_render_to_file(b, 'out/stand.scad', include_orig_code=True)
+screen = translate([-25.5, 0, 48])(
+    rotate([0, -60, 0])(
+        color([0, 0, 1])(boxes.roundedCube([68, 108, 12], 2, True, True))
+    )
+)
+
+
+rise_notched = rise - screen
+
+stand = union()(
+    base,
+    rise_notched,
+)
+
+if __name__ == "__main__":
+    file_out = scad_render_to_file(stand, "out/stand.scad", include_orig_code=True)
     print(f"{__file__}: SCAD file written to: \n{file_out}")
